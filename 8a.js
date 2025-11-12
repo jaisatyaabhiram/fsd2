bff659a442cf3d265dc44ca837c3e53fa3f32de4
Jai Satya Abhiram Mallidi <163704204+jaisatyaabhiram@users.noreply.github.com>
2025-11-12 20:54:26 +0530

    Fix comment syntax in 8a.js

---
+++ 8a.js
@@ -0,0 +1,11 @@
+//8(a) Functional Component
+
+Displays: “Welcome to React!”
+
+import React from 'react';
+
+function Welcome() {
+  return <h1>Welcome to React!</h1>;
+}
+
+export default Welcome;
